089b81306124f4e2a19667edbaaa920d787aa381
Максим <92673929+Izike1@users.noreply.github.com>
2023-07-08 12:52:43 +0300

    Update js.js

--- gulp/task/js.js
+++ gulp/task/js.js
@@ -1,20 +1,25 @@
 import webpack from "webpack-stream";
 
 export const js = () => {
-    return app.gulp.src(app.path.src.js, {
-            sourcemaps: app.isDev
-        })
+    return app.gulp.src([app.path.src.js, app.path.src.scriptJs], {
+        sourcemaps: app.isDev
+    })
         .pipe(app.plugins.plumber(
             app.plugins.notify.onError({
                 title: "js",
                 message: "Error: <%= error.message %>"
-            })))
+            })
+        ))
         .pipe(webpack({
             mode: app.isBuild ? 'production' : 'development',
+            entry: {
+                app: app.path.src.js,
+                script: app.path.src.scriptJs,
+            },
             output: {
-                filename: 'app.min.js',
+                filename: '[name].min.js',
             }
         }))
         .pipe(app.gulp.dest(app.path.build.js))
         .pipe(app.plugins.browsersync.stream());
-}
+};
